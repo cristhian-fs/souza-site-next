9348bd8196bb8fef28342e052ff41c267df199b2
Cristhian <cristhianfernandolp@gmail.com>
2024-01-22 20:15:15 -0300

    New taiwindcss and package.json configs and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1216px",
+      },
+      fontFamily: {
+        sans: ['"Suisse Intl"', "helvetica", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
     },
   },
   plugins: [],
